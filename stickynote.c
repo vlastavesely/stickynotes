996c3193953758b22778f27d59e641dbccad5454
Vlasta Vesely <vlastavesely@protonmail.ch>
2022-08-15 15:26:54 +0200

    stickynote.c: implement saving geometry

--- stickynote.c
+++ stickynote.c
@@ -5,6 +5,10 @@ struct StickyNote {
 	GtkWindow parent;
 	GSettings *settings;
 	GtkWidget *title_label;
+	GtkWidget *move_box;
+	GtkWidget *resize_sw;
+	GtkWidget *resize_se;
+	int width, height, x, y;
 };
 
 struct StickyNoteClass {
@@ -14,6 +18,10 @@ struct StickyNoteClass {
 enum {
 	PROP_NAME = 1,
 	PROP_TITLE,
+	PROP_WIDTH,
+	PROP_HEIGHT,
+	PROP_X,
+	PROP_Y,
 	N_PROPERTIES
 };
 
@@ -37,6 +45,65 @@ static void load_settings_for_name(StickyNote *note, const char *name)
 	note->settings = settings;
 }
 
+static bool stickynote_resize(GtkWidget *widget, GdkEventButton *event,
+			      StickyNote *note)
+{
+	unsigned int edge;
+
+	if (event->type != GDK_BUTTON_PRESS || event->button != 1)
+		return false;
+
+	edge = (widget == note->resize_se) ? GDK_WINDOW_EDGE_SOUTH_EAST
+		: GDK_WINDOW_EDGE_SOUTH_WEST;
+
+	gtk_window_begin_resize_drag(GTK_WINDOW(note), edge, event->button,
+				     event->x_root, event->y_root,
+				     event->time);
+
+	return true;
+}
+
+static bool stickynote_move(GtkWidget *widget, GdkEventButton *event,
+			    StickyNote *note)
+{
+	if (event->type != GDK_BUTTON_PRESS || event->button != 1)
+		return false;
+
+	gtk_window_begin_move_drag(GTK_WINDOW(note), event->button,
+				   event->x_root, event->y_root,
+				   event->time);
+
+	return true;
+}
+
+static bool save_geometry(GtkWidget *widget, GdkEventConfigure *event, void *data)
+{
+	GObject *object = G_OBJECT(widget);
+	StickyNote *note = STICKYNOTE(widget);
+
+	note->width = event->width;
+	g_object_notify(object, "width");
+
+	note->height = event->height;
+	g_object_notify(object, "height");
+
+	note->x = event->x;
+	g_object_notify(object, "x");
+
+	note->y = event->y;
+	g_object_notify(object, "y");
+
+	return false;
+}
+
+static void update_geometry(StickyNote *note)
+{
+	GtkWindow *window = GTK_WINDOW(note);
+
+	gtk_window_resize(window, note->width, note->height);
+	gtk_window_move(window, note->x, note->y);
+}
+
 static void set_property(GObject *object, unsigned int prop_id,
 			 const GValue *value, GParamSpec *pspec)
 {
@@ -51,6 +118,26 @@ static void set_property(GObject *object, unsigned int prop_id,
 	case PROP_TITLE:
 		str = g_value_get_string(value);
 		gtk_label_set_label(GTK_LABEL(note->title_label), str);
+		break;
+
+	case PROP_WIDTH:
+		note->width = g_value_get_int(value);
+		update_geometry(note);
+		break;
+
+	case PROP_HEIGHT:
+		note->height = g_value_get_int(value);
+		update_geometry(note);
+		break;
+
+	case PROP_X:
+		note->x = g_value_get_int(value);
+		update_geometry(note);
+		break;
+
+	case PROP_Y:
+		note->y = g_value_get_int(value);
+		update_geometry(note);
 		break;
 
 	default:
@@ -71,6 +158,22 @@ static void get_property(GObject *object, unsigned int prop_id, GValue *value,
 		g_value_set_string(value, str);
 		break;
 
+	case PROP_WIDTH:
+		g_value_set_int(value, note->width);
+		break;
+
+	case PROP_HEIGHT:
+		g_value_set_int(value, note->height);
+		break;
+
+	case PROP_X:
+		g_value_set_int(value, note->x);
+		break;
+
+	case PROP_Y:
+		g_value_set_int(value, note->y);
+		break;
+
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
 		break;
@@ -80,6 +183,22 @@ static void get_property(GObject *object, unsigned int prop_id, GValue *value,
 static void stickynote_init(StickyNote *note)
 {
 	gtk_widget_init_template(GTK_WIDGET(note));
+
+	g_signal_connect(G_OBJECT(note->resize_sw), "button-press-event",
+			 G_CALLBACK(stickynote_resize), note);
+
+	g_signal_connect(G_OBJECT(note->resize_se), "button-press-event",
+			 G_CALLBACK(stickynote_resize), note);
+
+	g_signal_connect(G_OBJECT(note->move_box), "button-press-event",
+			 G_CALLBACK(stickynote_move), note);
+
+	g_signal_connect(G_OBJECT(note), "configure-event",
+			 G_CALLBACK(save_geometry), NULL);
+
+	/* Minimal default */
+	note->width = 100;
+	note->height = 100;
 }
 
 static void finalise(GObject *object)
@@ -98,6 +217,18 @@ static void constructed(GObject *object)
 
 	g_settings_bind(settings, "title", note, "title",
 			G_SETTINGS_BIND_DEFAULT);
+
+	g_settings_bind(settings, "width", note, "width",
+			G_SETTINGS_BIND_DEFAULT);
+
+	g_settings_bind(settings, "height", note, "height",
+			G_SETTINGS_BIND_DEFAULT);
+
+	g_settings_bind(settings, "x", note, "x",
+			G_SETTINGS_BIND_DEFAULT);
+
+	g_settings_bind(settings, "y", note, "y",
+			G_SETTINGS_BIND_DEFAULT);
 }
 
 static void stickynote_class_init(StickyNoteClass *klass)
@@ -111,6 +242,13 @@ static void stickynote_class_init(StickyNoteClass *klass)
 	gtk_widget_class_bind_template_child(widget_class,
 					     StickyNote, title_label);
 
+	gtk_widget_class_bind_template_child(widget_class,
+					     StickyNote, move_box);
+	gtk_widget_class_bind_template_child(widget_class,
+					     StickyNote, resize_sw);
+	gtk_widget_class_bind_template_child(widget_class,
+					     StickyNote, resize_se);
+
 	object_class->get_property = get_property;
 	object_class->set_property = set_property;
 	object_class->finalize = finalise;
@@ -122,6 +260,22 @@ static void stickynote_class_init(StickyNoteClass *klass)
 
 	obj_properties[PROP_TITLE] = g_param_spec_string("title",
 			"Title", "Public title of the note", "",
+			G_PARAM_READABLE | G_PARAM_WRITABLE);
+
+	obj_properties[PROP_WIDTH] = g_param_spec_int("width",
+			"Width", "Width of the note", 100, 3000, 100,
+			G_PARAM_READABLE | G_PARAM_WRITABLE);
+
+	obj_properties[PROP_HEIGHT] = g_param_spec_int("height",
+			"Height", "Height of the note", 100, 3000, 100,
+			G_PARAM_READABLE | G_PARAM_WRITABLE);
+
+	obj_properties[PROP_X] = g_param_spec_int("x",
+			"X", "Offset from the left", -5000, 5000, 100,
+			G_PARAM_READABLE | G_PARAM_WRITABLE);
+
+	obj_properties[PROP_Y] = g_param_spec_int("y",
+			"Y", "Offset from the top", -5000, 5000, 100,
 			G_PARAM_READABLE | G_PARAM_WRITABLE);
 
 	g_object_class_install_properties(object_class, N_PROPERTIES,
